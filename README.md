1bf99d52aeda08ef1e43060ef39bfe6e2cadaa2a
Peter Heimig <peter.heimig@agindo.de>
2019-03-04 23:35:05 +0100

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
- 
+# Microsoft Graph Transformer
+
+> Microsoft Graph Transformer for Open Integration Hub.
+
+## Usage
+
+1. register your app at https://portal.azure.com/
+2. set redirect uri to https://app.elastic.io/callback/oauth2
+3. set permissions **user.read, email, offline_access, openid, profile**
+4. set environment variables on elastic.io **MSAPP_CLIENT_ID, MSAPP_CLIENT_SECRET**
+
+----------
+
+*Copyright (c) 2019 agindo GmbH.*
--- verifyCredentials.js
+++ verifyCredentials.js
@@ -1,1 +1,29 @@
- 
+const MicrosoftGraph = require("msgraph-sdk-javascript");
+
+const co = require("co");
+
+module.exports = function verifyCredentials(credentials, cb)
+{
+    var client = MicrosoftGraph.init
+    ({
+        defaultVersion: "v1.0",
+        debugLogging: false,
+        authProvider: (done) =>
+        {
+            done(null, credentials.oauth.access_token);
+        }
+    });
+
+    var process = co(function*()
+    {
+        var user = yield client.api("/me").get();
+    });
+
+    process.then(function ()
+    {
+        cb(null, {verified: true});
+    }).catch(err =>
+    {
+        cb(null, {verified: false});
+    });
+};
