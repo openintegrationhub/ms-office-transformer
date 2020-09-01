64b72a13f9cf3a88a264d8f0a14d08f0793fae1a
jkopen <jknoop@wice.de>
2020-09-01 12:26:04 +0200

    Adding gitignore and basic structure for tests

---
+++ test/base.test.js
@@ -0,0 +1,12 @@
+const { expect } = require('chai');
+
+describe('transform from oih data to microsoft', () => {
+  before(async () => {
+
+  });
+
+  it('should do some testing', async () => {
+    // expect(result).to.be.an('object');
+  });
+
+});
